5b86ce0ac3e6e2b9a09c68828ea90032fc75ca29
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-02-17 16:26:34 +0700

    add productsController & productsRoutes

--- server/controllers/productsControllers.js
+++ server/controllers/productsControllers.js
@@ -14,3 +14,81 @@ export const createNewProduct = async (req, res) => {
     });
   };
 };
+
+export const readAllProducts = async (req, res) => {
+  try {
+    const allProducts = await Product.find();
+    return res.json({
+      message: "All products fetched!",
+      data: allProducts
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  };
+};
+
+export const readSingleProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById({ _id: id });
+    if(!product) {
+      return res.status(404).json({
+        message: "Product not found!"
+      });
+    };
+    return res.json({
+      message: "Product found!",
+      data: product
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  };
+};
+
+export const updateSingleProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, price, stock, status, imageUrl } = req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(id, {
+      name, price, stock, status, imageUrl
+    });
+
+    if(!updatedProduct) {
+      return res.status(404).json({
+        message: "Product not found!"
+      });
+    };
+
+    return res.json({
+      message: "Product updated successfully!",
+      data: updatedProduct
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  };
+};
+
+export const deleteSingleProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById({ _id: id });
+    console.log(product);
+    if(!product) {
+      return res.status(404).json({
+        message: "Product not found!"
+      });
+    };
+    await Product.findByIdAndDelete({ _id: id });
+    return res.json({ message: "Product deleted successfully!" });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  };
+};
--- server/routes/productsRoutes.js
+++ server/routes/productsRoutes.js
@@ -1,9 +1,21 @@
 import express from 'express';
-import { createNewProduct } from '../controllers/productsControllers.js';
+import { createNewProduct, readAllProducts, readSingleProduct, updateSingleProduct, deleteSingleProduct } from '../controllers/productsControllers.js';
 
 const router = express.Router();
 
 // CREATE NEW PRODUCT
 router.post('/', createNewProduct);
+
+// READ ALL PRODUCTS
+router.get('/', readAllProducts);
+
+// READ SINGLE PRODUCT
+router.get('/:id', readSingleProduct);
+
+// UPDATE SINGLE PRODUCT
+router.put('/:id', updateSingleProduct);
+
+// DELETE SINGLE PRODUCT
+router.delete(':/id', deleteSingleProduct);
 
 export default router;
