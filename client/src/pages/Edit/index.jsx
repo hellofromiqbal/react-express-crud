ad5e23c5aaf7e35a353ff81c7af834dada9e2170
Muhammad Iqbal <hellofromiqbal@gmail.com>
2024-02-19 16:22:14 +0700

    add edit product feature

--- client/src/pages/Edit/index.jsx
+++ client/src/pages/Edit/index.jsx
@@ -1,17 +1,91 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import Input from '../../components/Input'
 
 const Edit = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [formState, setFormState] = useState({
+    name: '',
+    price: 0,
+    stock: 0,
+    status: true  
+  });
+  useEffect(() => {
+    fetch(`http://localhost:5173/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const result = data.data;
+        setFormState({
+          name: result.name,
+          price: result.price,
+          stock: result.stock,
+          status: result.status
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const formSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`http://localhost:5173/products/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(formState)
+      });
+      if(!res.ok) {
+        const result = await res.json();
+        throw new Error(result.message);
+      } else {
+        const result = await res.json();
+        alert(result.message);
+        navigate("/");
+      };
+    } catch (error) {
+      alert(error.message);
+    };
+  };
+
   return (
     <div className="main">
       <div className="card">
         <h2>Edit Produk</h2>
         <br />
-        <form>
-          <Input name="name" type="text" placeholder="Nama Produk..." label="Nama" value="test"/>
-          <Input name="price" type="number" placeholder="Harga Produk..." label="Harga" value="20000000"/>
-          <Input name="Stock" type="number" placeholder="Stock Produk..." label="Stock" value="10"/>
-          <Input name="status" type="checkbox" label="Active" checked/>
+        <form onSubmit={formSubmit}>
+          <Input
+            name="name"
+            type="text"
+            placeholder="Nama Produk..."
+            label="Nama"
+            value={formState.name}
+            onChange={(e) => setFormState({ ...formState, name: e.target.value })}
+          />
+          <Input
+            name="price"
+            type="number"
+            placeholder="Harga Produk..."
+            label="Harga"
+            value={formState.price}
+            onChange={(e) => setFormState({ ...formState, price: e.target.value })}
+          />
+          <Input
+            name="Stock"
+            type="number"
+            placeholder="Stock Produk..."
+            label="Stock"
+            value={formState.stock}
+            onChange={(e) => setFormState({ ...formState, stock: e.target.value })}
+          />
+          <Input
+            name="status"
+            type="checkbox"
+            label="Active"
+            checked={formState.status}
+            onChange={() => setFormState({ ...formState, status: !formState.status })}
+          />
           <button type="submit" className="btn btn-primary">Simpan</button>
         </form>
       </div>
